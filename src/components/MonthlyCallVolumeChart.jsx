77058657389ddd00bbacf2428530a135a179e2dd
cchestnutc <128385289+cchestnutc@users.noreply.github.com>
2026-03-23 14:31:09 -0500

    Add MonthlyCallVolumeChart component

---
+++ src/components/MonthlyCallVolumeChart.jsx
@@ -0,0 +1,87 @@
+import React, { useMemo } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  ResponsiveContainer,
+  Legend,
+  LabelList,
+} from "recharts";
+import { buildMonthlyChartDataFromAggregateDocs } from "../utils/phoneDashboardData";
+
+const MonthlyCallVolumeChart = ({
+  aggregateDocs = [],
+  title = "Call Volume",
+}) => {
+  const currentYear = new Date().getFullYear();
+
+  const chartData = useMemo(() => {
+    return buildMonthlyChartDataFromAggregateDocs(aggregateDocs);
+  }, [aggregateDocs]);
+
+  const sortedYears = useMemo(() => {
+    return Array.from(new Set(aggregateDocs.map((doc) => doc.year))).sort((a, b) => a - b);
+  }, [aggregateDocs]);
+
+  const getYearColor = (year) => {
+    if (year === currentYear) return "#59a14f";
+    if (year === currentYear - 1) return "#f28e2c";
+
+    const yearColors = ["#4e79a7", "#e15759", "#b07aa1", "#76b7b2", "#edc949"];
+    const index = sortedYears.indexOf(year) % yearColors.length;
+    return yearColors[index];
+  };
+
+  return (
+    <div className="monthly-chart">
+      <div
+        style={{
+          fontWeight: 600,
+          fontSize: "1rem",
+          textAlign: "center",
+          marginBottom: "0.5rem",
+          color: "#111827",
+        }}
+      >
+        {title}
+      </div>
+
+      <div style={{ width: "100%", height: "450px" }}>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            data={chartData}
+            margin={{ top: 20, right: 20, left: 10, bottom: 30 }}
+            barGap={2}
+            barCategoryGap="15%"
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="month"
+              angle={0}
+              textAnchor="middle"
+              height={60}
+            />
+            <YAxis />
+            <Tooltip />
+            <Legend verticalAlign="top" height={36} />
+            {sortedYears.map((year) => (
+              <Bar
+                key={year}
+                dataKey={year}
+                fill={getYearColor(year)}
+                isAnimationActive={false}
+              >
+                <LabelList dataKey={year} position="top" />
+              </Bar>
+            ))}
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+};
+
+export default MonthlyCallVolumeChart;
